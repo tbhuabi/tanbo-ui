3b9b5d76a701596d94258173747e6dc546acba86
tanbo <464328895@qq.com>
2017-05-04 20:43:12 +0800

    checkbox

--- src/views/app.ts
+++ src/views/app.ts
@@ -5,32 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: 'app.html'
 })
 export class AppComponent {
-    open: boolean = false;
-    data: any = {
-        a: '',
-        b: ''
-    };
-    dataList: Array<any> = [{
-        key: 'name133333333333333333333333333333333333333333333333333333333333333333333333333333333333',
-        value: '000'
-    }, {
-        key: 'name2',
-        value: '11111'
-    }, {
-        key: 'name3',
-        value: '333'
-    }, {
-        key: 'name4',
-        value: '444'
-    }, {
-        key: 'name5',
-        value: '666'
-    }];
-
-    show(value) {
-        console.log(value);
-    }
+    data: any = {a: '1'};
+    list: Array<string> = ['1', '2', '3'];
 
     submit() {
+        console.log(this.data);
     }
 }
