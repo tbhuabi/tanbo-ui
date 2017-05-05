6ea00f0698aaddd4416df1ad68e18474069ab212
tanbo <464328895@qq.com>
2017-05-05 21:20:31 +0800

    去除不必要的依赖

--- src/modules/ui-components/ui-components.module.ts
+++ src/modules/ui-components/ui-components.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UiDirectivesModule } from '../ui-directives/ui-directives.module';
 
 import { DropDownComponent } from './components/drop-down/drop-down.component';
 @NgModule({
     imports: [
-        BrowserAnimationsModule,
         CommonModule,
         UiDirectivesModule
     ],
