d0451a56fe3a0b9215289a304a8742f9570ea21d
tanbo <464328895@qq.com>
2017-05-07 16:27:35 +0800

    range基本完成

--- src/modules/ui-forms/components/range/range.component.ts
+++ src/modules/ui-forms/components/range/range.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, ViewChild, Output, ElementRef, EventEmitter } from '@angular/core';
+import { EventManager } from '@angular/platform-browser';
 
 import { InputType } from '../../utils/input-type';
 
@@ -7,11 +8,58 @@ import { InputType } from '../../utils/input-type';
     templateUrl: './range.component.html'
 })
 export class RangeComponent implements InputType {
+    @ViewChild('rangeBar') rangeBar: ElementRef;
+
     @Input()
-    disabled: boolean = false;
+    set disabled(isDisabled: any) {
+        this._disabled = isDisabled;
+    }
+
+    get disabled() {
+        let isDisabled = (this as any).hasOwnProperty('_disabled');
+        return isDisabled && this._disabled !== false;
+    }
+
     @Input()
-    readonly: boolean = false;
+    set readonly(isReadonly: any) {
+        this._readonly = isReadonly;
+    }
+
+    get readonly() {
+        let isReadonly = (this as any).hasOwnProperty('_readonly');
+        return isReadonly && this._readonly !== false;
+    }
 
     @Output()
     change = new EventEmitter<number>();
+
+    position: number = 50;
+
+    private _disabled: boolean;
+    private _readonly: boolean;
+
+    constructor(private elementRef: ElementRef,
+                private eventManager: EventManager) {
+
+    }
+
+    drag(event: any) {
+        let maxWidth = this.elementRef.nativeElement.offsetWidth;
+        let nowWidth = this.rangeBar.nativeElement.offsetWidth;
+        let oldX = event.clientX;
+        let mouseMoveUnbindFn = this.eventManager.addGlobalEventListener('document', 'mousemove', (ev) => {
+            let dragDistance = ev.clientX - oldX;
+            let p = (nowWidth + dragDistance) / maxWidth * 100;
+            if (p < 0) {
+                p = 0;
+            } else if (p > 100) {
+                p = 100;
+            }
+            this.position = p;
+        });
+        let moseUpUnbindFn = this.eventManager.addGlobalEventListener('document', 'mouseup', () => {
+            mouseMoveUnbindFn();
+            moseUpUnbindFn();
+        });
+    }
 }
