45cb813bab7ac556c071dbe6d72ff582b0509dd3
tanbo <464328895@qq.com>
2017-05-08 19:11:13 +0800

    tsc

---
+++ src/modules/index.ts
@@ -0,0 +1,4 @@
+export { UiChartsModule } from './ui-charts/ui-charts.module';
+export { UiFormsModule } from './ui-forms/ui-form.module';
+export { UiDirectivesModule } from './ui-directives/ui-directives.module';
+export { UiComponentsModule } from './ui-components/ui-components.module';
