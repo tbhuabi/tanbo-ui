9f862a99041b0d64e7c22cc27d32469d3cc54b7b
tanbo <464328895@qq.com>
2017-07-20 21:19:31 +0800

    ui-editor 表格bug

--- src/modules/ui-forms/components/editor/editor.component.ts
+++ src/modules/ui-forms/components/editor/editor.component.ts
@@ -160,7 +160,7 @@ export class EditorComponent implements AfterViewInit, ControlValueAccessor {
         let template = `
 
 | 项目        | 价格   |  数量  |
-| --------   | -----:  | :----:  |
+| --------   | -----  | ------ |
 | 计算机     | \$1600 |   5     |
 | 手机        |   \$12   |   12   |
 | 管线        |    \$1    |  234  |
