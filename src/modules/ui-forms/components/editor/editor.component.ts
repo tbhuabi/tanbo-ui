79e625ae98295f5351a6a91583ecb5afd86145e9
tanbo <464328895@qq.com>
2017-09-18 15:38:25 +0800

    ui-editor 回车换行bug修复

--- src/modules/ui-forms/components/editor/editor.component.ts
+++ src/modules/ui-forms/components/editor/editor.component.ts
@@ -55,7 +55,7 @@ export class EditorComponent implements AfterViewInit, ControlValueAccessor {
             if (change.origin === 'setValue') {
                 return;
             }
-            let value = instance.getValue().replace(/\n\s?/g, '\n  ');
+            let value = instance.getValue().replace(/\s+\n/g, '  \n');
             if (this.onChange) {
                 this.onChange(value);
             }
