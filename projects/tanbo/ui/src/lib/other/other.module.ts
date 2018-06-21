d41b80ae6ca7687f06e514287a7a80544270f7ca
tanbo <464328895@qq.com>
2018-06-21 13:05:37 +0800

    添加 tooltip

--- projects/tanbo/ui/src/lib/other/other.module.ts
+++ projects/tanbo/ui/src/lib/other/other.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DividerComponent } from './divider/divider.component';
+import { TooltipComponent } from './tooltip/tooltip.component';
 import { ViewLoadingBarComponent } from './view-loading-bar/view-loading-bar.component';
+
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
   imports: [
@@ -10,11 +13,17 @@ import { ViewLoadingBarComponent } from './view-loading-bar/view-loading-bar.com
   ],
   declarations: [
     DividerComponent,
-    ViewLoadingBarComponent
+    TooltipComponent,
+    ViewLoadingBarComponent,
+    TooltipDirective
   ],
   exports: [
     DividerComponent,
-    ViewLoadingBarComponent
+    ViewLoadingBarComponent,
+    TooltipDirective
+  ],
+  entryComponents: [
+    TooltipComponent
   ]
 })
 export class UIOtherModule {
---
+++ projects/tanbo/ui/src/lib/other/tooltip/tooltip.directive.ts
@@ -0,0 +1,56 @@
+import {
+  Directive,
+  Input,
+  ElementRef,
+  OnDestroy,
+  ViewContainerRef,
+  HostListener,
+  ComponentFactoryResolver,
+  ComponentFactory, ComponentRef
+} from '@angular/core';
+
+import { TooltipComponent } from './tooltip.component';
+
+@Directive({
+  selector: '[ui-tooltip]'
+})
+export class TooltipDirective implements OnDestroy {
+  @Input('ui-tooltip')
+  tooltip = '';
+  @Input()
+  tooltipPosition: string = '';
+
+  private instance: TooltipComponent;
+  private tooltipComponent: ComponentRef<TooltipComponent>;
+  private tooltipFactory: ComponentFactory<TooltipComponent>;
+
+  constructor(private elementRef: ElementRef,
+              private componentFactoryResolver: ComponentFactoryResolver,
+              private viewContainerRef: ViewContainerRef) {
+  }
+
+  @HostListener('mouseenter')
+  mouseEnter() {
+    if (!this.instance) {
+      this.tooltipFactory = this.componentFactoryResolver.resolveComponentFactory(TooltipComponent);
+      this.tooltipComponent = this.viewContainerRef.createComponent(this.tooltipFactory);
+      this.instance = this.tooltipComponent.instance;
+    }
+    this.instance.text = this.tooltip;
+    this.instance.referenceElement = this.elementRef.nativeElement;
+    this.instance.show();
+  }
+
+  @HostListener('mouseleave')
+  mouseLeave() {
+    if (this.instance) {
+      this.instance.hide();
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.instance) {
+      this.tooltipComponent.destroy();
+    }
+  }
+}
