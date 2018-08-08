f258094894f05f13b882680ecb4e1eb96363f518
tanbo <464328895@qq.com>
2018-08-08 14:34:10 +0800

    优化btn样式

--- src/tanbo/ui/lib/layout/tab-view-item/tab-view-item.component.ts
+++ src/tanbo/ui/lib/layout/tab-view-item/tab-view-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, ChangeDetectorRef } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'ui-tab-view-item',
@@ -6,21 +6,5 @@ import { Component, HostBinding, ChangeDetectorRef } from '@angular/core';
 })
 export class TabViewItemComponent {
   @HostBinding('class.ui-active')
-  set active(value: boolean) {
-    this._active = value;
-    if (value) {
-      this.changeDetectorRef.reattach();
-    } else {
-      this.changeDetectorRef.detach();
-    }
-  }
-
-  get active() {
-    return this._active;
-  }
-
-  private _active: boolean = false;
-
-  constructor(private changeDetectorRef: ChangeDetectorRef) {
-  }
+  active = false;
 }
