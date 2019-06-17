830015a4c85e4d675dcfdcfd7e9dd91c5955daf0
tanbo <464328895@qq.com>
2019-06-17 10:31:10 +0800

    优化scroll组件

--- projects/tanbo/ui/src/lib/scroll/scroll/scroll.component.ts
+++ projects/tanbo/ui/src/lib/scroll/scroll/scroll.component.ts
@@ -1,12 +1,20 @@
-import { Component, ElementRef, HostListener, ViewChild, Input, Output, AfterViewInit, HostBinding, OnDestroy } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  ViewChild,
+  Input,
+  Output,
+  AfterViewInit,
+  HostBinding,
+  OnDestroy
+} from '@angular/core';
 
 @Component({
   selector: 'ui-scroll',
   templateUrl: './scroll.component.html'
 })
 export class ScrollComponent implements AfterViewInit, OnDestroy {
-  @Input() scrollTop = 0;
-  @Input() scrollLeft = 0;
   @HostBinding('class.ui-overflow-x')
   @Input() overflowX = false;
   @HostBinding('class.ui-overflow-y')
@@ -14,15 +22,46 @@ export class ScrollComponent implements AfterViewInit, OnDestroy {
   @ViewChild('content', {static: true}) content: ElementRef;
   @Input() scrollBarMinLength = 50;
 
+  @Input()
+  set scrollTop(v: number) {
+    if (v < 0) {
+      this._scrollTop = 0;
+    } else if (v > this.maxScrollHeight) {
+      this._scrollTop = this.maxScrollHeight;
+    } else {
+      this._scrollTop = v;
+    }
+  }
+
+  get scrollTop() {
+    return this._scrollTop;
+  }
+
+  @Input()
+  set scrollLeft(v: number) {
+    if (v < 0) {
+      this._scrollLeft = 0;
+    } else if (v > this.maxScrollWidth) {
+      this._scrollLeft = this.maxScrollWidth;
+    } else {
+      this._scrollLeft = v;
+    }
+  }
+
+  get scrollLeft() {
+    return this._scrollLeft;
+  }
+
   scrollYBarLength: number;
   scrollXBarLength: number;
 
   get transform() {
-    return `translateX(${this.transformX}px) translateY(${this.transformY}px)`;
+    return `translateX(${-this.scrollLeft}px) translateY(${-this.scrollTop}px)`;
   }
 
-  private transformX = 0;
-  private transformY = 0;
+  private _scrollLeft = 0;
+  private _scrollTop = 0;
+
   private topDistance = 0;
   private leftDistance = 0;
   private containerWidth: number;
@@ -30,8 +69,8 @@ export class ScrollComponent implements AfterViewInit, OnDestroy {
   private contentWidth: number;
   private contentHeight: number;
 
-  private maxScrollHeight: number;
-  private maxScrollWidth: number;
+  private maxScrollHeight = 0;
+  private maxScrollWidth = 0;
 
   private animateId: number;
 
@@ -47,8 +86,8 @@ export class ScrollComponent implements AfterViewInit, OnDestroy {
       this.containerWidth = containerElement.offsetWidth;
       this.contentHeight = contentElement.offsetHeight;
       this.contentWidth = contentElement.offsetWidth;
-      this.maxScrollHeight = this.containerHeight - this.contentHeight;
-      this.maxScrollWidth = this.containerWidth - this.contentWidth;
+      this.maxScrollHeight = this.contentHeight - this.containerHeight;
+      this.maxScrollWidth = this.contentWidth - this.containerWidth;
       this.animateId = requestAnimationFrame(fn);
     };
     fn();
@@ -65,27 +104,20 @@ export class ScrollComponent implements AfterViewInit, OnDestroy {
   @HostListener('mousewheel', ['$event'])
   scroll(event: WheelEvent) {
     if (this.overflowY) {
-      this.transformY = this.transformY - event.deltaY;
-      const maxScrollHeight = this.maxScrollHeight;
-      if (this.transformY > 0) {
-        this.transformY = 0;
-      } else if (this.transformY < maxScrollHeight) {
-        this.transformY = maxScrollHeight;
-      } else {
+      const scrollTop = this.scrollTop + event.deltaY;
+      this.scrollTop = scrollTop;
+      if (scrollTop >= 0 && scrollTop <= this.maxScrollHeight) {
         event.preventDefault();
       }
       this.setScrollBar();
     }
     if (this.overflowX) {
-      this.transformX = this.transformX - event.deltaX;
-      const maxScrollWidth = this.maxScrollWidth;
-      if (this.transformX > 0) {
-        this.transformX = 0;
-      } else if (this.transformX < maxScrollWidth) {
-        this.transformX = maxScrollWidth;
-      } else {
+      const scrollLeft = this.scrollLeft - event.deltaX;
+      this.scrollLeft = scrollLeft;
+      if (scrollLeft >= 0 && scrollLeft <= this.maxScrollWidth) {
         event.preventDefault();
       }
+      this.setScrollBar();
     }
   }
 
@@ -95,15 +127,15 @@ export class ScrollComponent implements AfterViewInit, OnDestroy {
       if (y < 1) {
         const scrollYBarLength = y * this.containerHeight;
         this.scrollYBarLength = scrollYBarLength < this.scrollBarMinLength ? this.scrollBarMinLength : scrollYBarLength;
-        this.topDistance = (this.containerHeight - this.scrollYBarLength) * (this.transformY / this.maxScrollHeight);
-        console.log(this.containerHeight, this.scrollYBarLength, this.transformY, this.maxScrollHeight);
+        this.topDistance = (this.containerHeight - this.scrollYBarLength) * (this.scrollTop / this.maxScrollHeight);
       }
     }
     if (this.overflowX) {
       const x = this.containerWidth / this.contentWidth;
       if (x < 1) {
         const scrollXBarLength = x * this.containerWidth;
-        this.scrollYBarLength = scrollXBarLength < this.scrollBarMinLength ? this.scrollYBarLength : scrollXBarLength;
+        this.scrollXBarLength = scrollXBarLength < this.scrollBarMinLength ? this.scrollBarMinLength : scrollXBarLength;
+        this.leftDistance = (this.containerWidth - this.scrollXBarLength) * (this.scrollLeft / this.maxScrollWidth);
       }
     }
   }
