a38423ceb4c57708ab56b602b3e1506b9de6c5fc
tanbo <464328895@qq.com>
2019-07-18 21:42:27 +0800

    dropdown bug修复

--- projects/tanbo/ui/src/lib/app/app/app.component.ts
+++ projects/tanbo/ui/src/lib/app/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 import { DropdownRenderer } from '../../dropdown/help';
 
