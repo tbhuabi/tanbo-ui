4c21df9b3ebb5cc0caa29c03d235d94347a1e23a
tanbo <464328895@qq.com>
2020-05-15 17:54:45 +0800

    下载功能更新

--- projects/tanbo/ui/src/lib/other/download.directive.ts
+++ projects/tanbo/ui/src/lib/other/download.directive.ts
@@ -1,56 +1,79 @@
 import { Directive, EventEmitter, HostListener, Input, Output } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpEventType, HttpResponse } from '@angular/common/http';
+import { filter } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Directive({
   selector: '[uiDownload]'
 })
 export class DownloadDirective {
   @Input()
-  api: string;
+  downloader: Observable<HttpEvent<Blob>>;
   @Input()
   filename: string;
   @Input()
   ext: string;
   @Input()
-  params: { [key: string]: string | string[] };
-  @Input()
   mime = '';
   @Input()
   endings: EndingType = 'transparent';
-  @Input()
-  headers: { [key: string]: string | string[] } | HttpHeaders;
 
   @Output()
-  loadingStart = new EventEmitter<void>();
+  uiLoadStartBefore = new EventEmitter<void>();
 
   @Output()
-  loaded = new EventEmitter<void>();
+  uiLoadingStart = new EventEmitter<void>();
 
-  constructor(private http: HttpClient) {
-  }
+  @Output()
+  uiLoading = new EventEmitter<number>();
+
+  @Output()
+  uiLoaded = new EventEmitter<void>();
+
+  @Output()
+  uiLoadError = new EventEmitter<Error>();
 
   @HostListener('click')
   download() {
-    this.loadingStart.emit();
-    this.http.get(this.api, {
-      headers: this.headers,
-      params: this.params,
-      responseType: 'blob'
-    }).subscribe(res => {
-      this.loaded.emit();
-      const file = new Blob([res], {
-        type: this.mime,
-        endings: this.endings
+    this.uiLoadStartBefore.emit();
+    setTimeout(() => {
+      if (!(this.downloader instanceof Observable)) {
+        return;
+      }
+      this.downloader.pipe(filter((event: HttpEvent<Blob>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            this.uiLoadingStart.emit();
+            return false;
+          case HttpEventType.UploadProgress:
+            const percentDone = Math.floor(100 * event.loaded / event.total);
+            this.uiLoading.emit(percentDone);
+            return false;
+          case HttpEventType.Response:
+            return true;
+          default:
+            return false;
+        }
+      })).subscribe(res => {
+        this.uiLoaded.emit();
+        const file = new Blob([(res as HttpResponse<Blob>).body], {
+          type: this.mime,
+          endings: this.endings
+        });
+        const url = URL.createObjectURL(file);
+        const a = document.createElement('a');
+        a.href = url;
+        a.target = '_blank';
+        if (this.filename) {
+          a.download = this.ext ? `${this.filename}.${this.ext}` : this.filename;
+        }
+        document.body.appendChild(a);
+        a.click();
+        URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+      }, error => {
+        this.uiLoadError.emit(error);
       });
-      const url = URL.createObjectURL(file);
-      const a = document.createElement('a');
-      a.href = url;
-      a.target = '_blank';
-      a.download = this.ext ? `${this.filename}.${this.ext}` : this.filename;
-      document.body.appendChild(a);
-      a.click();
-      URL.revokeObjectURL(url);
-      document.body.removeChild(a);
-    });
+    })
   }
 }
