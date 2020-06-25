22879837e8121be00df12864f44d1f0ba126fd86
tanbo <464328895@qq.com>
2020-06-25 10:03:41 +0800

    feat: update

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, TemplateRef, ViewChild } from '@angular/core';
 import { ModalController } from '@tanbo/ui/src/lib/modal/help';
+import { createEditor } from '@tanbo/tbus';
+import { Subject } from 'rxjs';
+
+class EventEmitter extends Subject<any> {
+  private v: any;
+  emit(v: any) {
+    this.v = v;
+  }
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('divElement')
+  divElement: ElementRef;
   @ViewChild('modal')
   modal: TemplateRef<any>;
   data = {
@@ -14,7 +25,13 @@ export class HomeComponent {
   };
   test = '';
 
+  onShow = new EventEmitter();
+
   constructor(private modalController: ModalController) {
+  }
+
+  ngAfterViewInit() {
+    // createEditor(this.divElement.nativeElement);
   }
 
   show() {
